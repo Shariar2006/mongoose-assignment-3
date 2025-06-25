63d1825d8f0c578bbd0b59f4ecd09cb0c38d84ae
Shariar2006 <shariarnafis86@gmail.com>
2025-06-25 18:42:55 +0600

    error message in update book

--- src/app/controllers/book.controller.ts
+++ src/app/controllers/book.controller.ts
@@ -30,15 +30,15 @@ bookRoutes.post('/', async (req: Request, res: Response) => {
 bookRoutes.get('/', async (req: Request, res: Response) => {
     try {
         const { filter, sortBy, sort, limit = '10' } = req.query;
-        
+
         const query: any = {};
         if (filter) {
-            query.genre = filter; 
+            query.genre = filter;
         }
-        
+
         const books = await Book.find(query)
-      .sort({ [sortBy as string]: sort === 'asc' ? 1 : -1 })
-      .limit(Number(limit));
+            .sort({ [sortBy as string]: sort === 'asc' ? 1 : -1 })
+            .limit(Number(limit));
 
         res.json({
             success: true,
@@ -58,7 +58,7 @@ bookRoutes.get('/', async (req: Request, res: Response) => {
 bookRoutes.get('/:bookId', async (req: Request, res: Response) => {
     try {
         const bookId = req.params.bookId
-        
+
         const book = await Book.findById(bookId)
 
         res.json({
@@ -80,18 +80,25 @@ bookRoutes.patch('/:bookId', async (req: Request, res: Response) => {
     try {
         const bookId = req.params.bookId
         const body = req.body
-        
-        if(body?.copies > 0){
-            body.available = true
-        }
-        
-        const book = await Book.findByIdAndUpdate(bookId, body, {new:true})
 
-        res.json({
-            success: true,
-            message: "Books updated successfully",
-            data: book
-        })
+        if (body?.copies < 0) {
+            res.status(400).json({
+                success: false,
+                message: 'Copies must be a positive number',
+            })
+        } else {
+            if (body?.copies > 0) {
+                body.available = true
+            }
+
+            const book = await Book.findByIdAndUpdate(bookId, body, { new: true })
+
+            res.json({
+                success: true,
+                message: "Books updated successfully",
+                data: book
+            })
+        }
     } catch (error: any) {
         res.status(400).json({
             message: error.message,
@@ -105,7 +112,7 @@ bookRoutes.patch('/:bookId', async (req: Request, res: Response) => {
 bookRoutes.delete('/:bookId', async (req: Request, res: Response) => {
     try {
         const bookId = req.params.bookId
-        
+
         await Book.findByIdAndDelete(bookId)
 
         res.json({
