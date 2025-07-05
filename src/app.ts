8b2242aed786cb455c5a83f38aa32b1f735231be
Shariar2006 <shariarnafis86@gmail.com>
2025-07-05 18:56:14 +0600

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express, { Application, Request, Response } from 'express'
 import { bookRoutes } from './app/controllers/book.controller'
 import { borrowRoutes } from './app/controllers/borrow.controller'
+import cors from 'cors'
 
 const app: Application = express()
 
+// cors
+app.use(cors({origin: 'http://localhost:5173'}))
 app.use(express.json())
 
 // routes
